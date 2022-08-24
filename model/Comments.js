913de8fac84157a98c2aaf2a8c5633f92d6cee6d
i.kłusek <J6ZkqwVUn>
2022-08-24 12:41:19 +0200

    Reafactor Comments logic: Comments are now saved to given product Document in comments collection

--- model/Comments.js
+++ model/Comments.js
@@ -3,24 +3,28 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
     productId: String,
-    userId: String,
-    userName: String,
-    date: String,
-    confirmed: Boolean,
-    likes: {
-        up: {
-            type: Number,
-            default: 0,
+    comments: [
+        {
+            userId: String,
+            userName: String,
+            date: String,
+            confirmed: Boolean,
+            likes: {
+                up: {
+                    type: Number,
+                    default: 0,
+                },
+                down: {
+                    type: Number,
+                    default: 0,
+                },
+            },
+            content: {
+                rating: Number,
+                description: String,
+            },
         },
-        down: {
-            type: Number,
-            default: 0,
-        },
-    },
-    content: {
-        rating: Number,
-        description: String,
-    },
+    ],
 });
 
 module.exports = mongoose.model('comments', commentSchema);
--- routes/api/comments.js
+++ routes/api/comments.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const commentsController = require('../../controllers/commentsController');
 
 router.route('/add').post(commentsController.addComment);
+router.route('/like').post(commentsController.likeComment);
 
 module.exports = router;
