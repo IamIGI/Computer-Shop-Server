110ff8009562d43af3f31597b7a43c30cd6c12ed
i.kłusek <J6ZkqwVUn>
2022-09-11 20:39:37 +0200

    remove console.log

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -5,7 +5,6 @@ const getAboutPageData = async (req, res) => {
     console.log(`${req.originalUrl}`);
     try {
         const response = await Contents.findOne({ pageName: 'About' }).lean();
-        console.log(response);
         return res.status(200).json(response);
     } catch (err) {
         console.log(err);
