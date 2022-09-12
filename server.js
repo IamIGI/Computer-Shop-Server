5a1dcf4d10c5fbfb7167a6bd0f6c864905224a26
i.kłusek <J6ZkqwVUn>
2022-09-12 23:12:21 +0200

    server testt v2

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ const { default: mongoose } = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 
-app.use((req, res) => {
-    console.log(req.path);
-});
-
 //Connect to MongoDB
 connectDB();
 
