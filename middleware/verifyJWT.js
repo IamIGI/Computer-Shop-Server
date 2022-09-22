584e246efb92da5def619906fa108e9e8b863ba9
i.kłusek <J6ZkqwVUn>
2022-09-22 23:19:05 +0200

    prod test6

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -3,7 +3,8 @@ const path = require('path');
 require('dotenv').config(path.join(__dirname, '..', '.env'));
 
 const verifyJWT = (req, res, next) => {
-    console.log(req);
+    console.log(req.originalUrl);
+    console.log(req.url);
     const authHeader = req.headers.authorization || req.headers.Authorization;
     console.log(`Token: ${authHeader}`);
     if (!authHeader?.startsWith('Bearer ')) return res.status(401).json({ message: 'missing authorization token' });
