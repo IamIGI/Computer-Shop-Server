7c78227c87169d5adae92b5ee14ebea9b6bd0f49
i.kłusek <J6ZkqwVUn>
2022-09-22 22:56:36 +0200

    prod test1

--- routes/api/HotShoot.js
+++ routes/api/HotShoot.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const hotShootController = require('../../controllers/hotShootController');
 
 router.route('/').get(hotShootController.getHotShoot);
-router.route('/get').get(hotShootController.getHotShoot);
 router.route('/timerchange').get(hotShootController.changeHotShootTimer);
+router.route('/get').get(hotShootController.getHotShoot);
 
 module.exports = router;
