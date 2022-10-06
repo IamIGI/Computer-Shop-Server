978b88d21de95b577ba45985481e2c8633ecdde9
i.kłusek <J6ZkqwVUn>
2022-10-06 15:46:12 +0200

    add image to message

--- routes/api/contact.js
+++ routes/api/contact.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const contactController = require('../../controllers/contactController');
 const fileUpload = require('express-fileupload');
-const filesPayloadExists = require('../../middleware/fileUpload/filesPayloadExists');
 const fileExtLimiter = require('../../middleware/fileUpload/fileExtLimiter');
 const fileSizeLimiter = require('../../middleware/fileUpload/fileSizeLimiter');
 
@@ -10,7 +9,6 @@ router
     .route('/sendmessage')
     .post(
         fileUpload({ createParentPath: true }),
-        filesPayloadExists,
         fileExtLimiter(['.png', '.jpg', 'jpeg']),
         fileSizeLimiter,
         contactController.sendMessage
