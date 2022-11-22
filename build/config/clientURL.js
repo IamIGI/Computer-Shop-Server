d0a9868412dbffbd0592478b1214ad1deadc95f5
i.kłusek <J6ZkqwVUn>
2022-11-22 08:48:02 +0100

    added env variable for "is it prod server" query

--- build/config/clientURL.js
+++ build/config/clientURL.js
@@ -7,5 +7,5 @@ const path_1 = __importDefault(require("path"));
 require('dotenv').config({ path: path_1.default.join(__dirname, '.env') });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const CLIENT_URL = app.get('env') === 'development' ? 'http://localhost:3000' : 'https://hotshoot.tk';
+const CLIENT_URL = process.env.DEPLOYMENTY === 'dev' ? 'http://localhost:3000' : 'https://hotshoot.tk';
 exports.default = CLIENT_URL;
--- src/config/clientURL.ts
+++ src/config/clientURL.ts
@@ -3,6 +3,5 @@ require('dotenv').config({ path: path.join(__dirname, '.env') });
 import express from 'express';
 const app = express();
 
-const CLIENT_URL = app.get('env') === 'development' ? 'http://localhost:3000' : 'https://hotshoot.tk';
-
+const CLIENT_URL = process.env.DEPLOYMENTY! === 'dev' ? 'http://localhost:3000' : 'https://hotshoot.tk';
 export default CLIENT_URL;
