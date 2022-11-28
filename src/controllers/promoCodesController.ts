6161fd79271d0570fd0034061c06fd3f2ad7e818
i.kłusek <J6ZkqwVUn>
2022-11-28 15:23:21 +0100

    add and Validate PromoCodes - rebuild

--- src/controllers/promoCodesController.ts
+++ src/controllers/promoCodesController.ts
@@ -4,24 +4,18 @@ import promoCodesServices from '../services/promoCodes.services';
 
 const addPromoCodes = async (req: Request, res: Response) => {
     console.log(`${req.originalUrl}`);
-    const { category, product, code } = req.body; //category: general , category: delivery, category : products
+    const { category, product, code, expiredIn } = req.body; //category: general , category: delivery, category : products
 
     try {
-        const response = await promoCodesServices.updatePromoCodes(category, product, code);
-        if ('err' in response) return res.status(400).json(response);
+        if (await promoCodesServices.checkIfPromoCodeExists(code))
+            return res.status(400).json({ message: 'Code already exists in db' });
 
+        await promoCodesServices.addPromoCodes(category, product, code, expiredIn);
         return res.status(200).json({ message: 'New promo code added to db' });
     } catch (err) {
         apiErrorHandler(req, res, err as Error);
     }
 };
-
-//when user send promo codes, we will first check for code in given product, then in general
-//all codes for delivery have to start with DELIVERY_ name
-//all codes have to have name which begin with category, for example :
-// delivery = DELIVERY_<code>
-// product  = PRODUCT_<code>
-// no category = _<code>
 
 const getPromoCodes = async (req: Request, res: Response) => {
     console.log(`${req.originalUrl}`);
