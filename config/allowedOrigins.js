6c4ed40da103307460c106a204471a32a39957a7
i.kłusek <J6ZkqwVUn>
2023-01-01 20:45:09 +0100

    Update 0.98

---
+++ config/allowedOrigins.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const allowedOrigins = [
+    'http://127.0.0.1:5000',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:5173',
+    'http://localhost:4173',
+    'https://hotshot.tk',
+    'https://hotshoot.tk', //PROD
+];
+exports.default = allowedOrigins;
